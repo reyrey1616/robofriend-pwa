37ec5639abaca6e45563e1341d91052ba4742d93
yobbad16 <dabboyrey@gmail.com>
2020-05-25 03:12:29 +0800

    Finished the counter button with simulation testing

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -4,27 +4,26 @@ class CounterButton extends PureComponent {
   constructor() {
     super();
     this.state = {
-      count: 0
-    }
+      count: 0,
+    };
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
-      return true
+      return true;
     }
-    return false
+    return false;
   }
 
   updateCount = () => {
-    this.setState(state => {
-      return {count: state.count + 1}
-    })
-  }
+    this.setState((state) => {
+      return { count: state.count + 1 };
+    });
+  };
 
   render() {
-    console.log('CounterButton')
     return (
-      <button color={this.props.color} onClick={this.updateCount}>
+      <button color={this.props.color} onClick={this.updateCount} id='counter'>
         Count: {this.state.count}
       </button>
     );
--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,5 +3,17 @@ import React from 'react';
 import CounterButton from './CounterButton';
 
 it('Renders the Counter Button Component', () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+  const mockColor = 'red';
+  expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
+});
+
+it('Correctly increments the counter', () => {
+  const mockColor = 'red';
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+
+  //   Simulate click test
+  wrapper.find('[id="counter"]').simulate('click');
+
+  expect(wrapper.state()).toEqual({ count: 1 });
+  expect(wrapper.props().color).toEqual('red');
 });
